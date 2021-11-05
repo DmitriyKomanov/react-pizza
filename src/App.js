7ed962f610f62accb4d6bab9b1df351104beeb13
komanov <komanov@twinscom.ru>
2021-11-05 17:55:33 +0300

    переделка редакса на функ.компонент и хуки. Часть 1

--- src/App.js
+++ src/App.js
@@ -1,65 +1,71 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-import React /*useEffect, useState*/ from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router';
 import { Header } from './components/';
 import { Cart, Home } from './pages';
 import axios from 'axios';
-import store from './redux/store';
-import { connect } from 'react-redux';
-import { setPizzas as setPizzasAction } from './redux/actions/pizzas';
+import { setPizzas } from './redux/actions/pizzas';
+import { useSelector, useDispatch } from 'react-redux';
 
-// function App() {
-// const [pizzas, setPizzas] = useState([]);
-// useEffect(() => {
-//   axios
-//     .get('http://localhost:3000/db.json')
-//     .then(({ data }) => setPizzas(data.pizzas));
-// }, []);
+const App = () => {
+  const dispatch = useDispatch(setPizzas);
 
-//   return (
-//     <div className='wrapper'>
-//       <Header />
-//       <div className='content'>
-//         <Route path='/' render={() => <Home items={pizzas} />} exact />
-//         <Route path='/cart' component={Cart} exact />
+  const state = useSelector(({ pizzas, filters }) => {
+    return { items: pizzas.items, sortBy: filters.sortBy };
+  });
+
+  console.log(state);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      dispatch(setPizzas(data.pizzas));
+    });
+  }, []);
+
+  return (
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <Route path='/' render={() => <Home items={[]} />} exact />
+        <Route path='/cart' component={Cart} exact />
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+// class App extends React.Component {
+//   componentDidMount() {
+//     axios
+//       .get('http://localhost:3000/db.json')
+//       .then(({ data }) => this.props.setPizzas(data.pizzas));
+//   }
+//   render() {
+//     console.log(this.props);
+//     return (
+//       <div className='wrapper'>
+//         <Header />
+//         <div className='content'>
+//           <Route
+//             path='/'
+//             render={() => <Home items={this.props.items} />}
+//             exact
+//           />
+//           <Route path='/cart' component={Cart} exact />
+//         </div>
 //       </div>
-//     </div>
-//   );
+//     );
+//   }
 // }
 
-class App extends React.Component {
-  componentDidMount() {
-    axios
-      .get('http://localhost:3000/db.json')
-      .then(({ data }) => this.props.setPizzas(data.pizzas));
-  }
-  render() {
-    console.log(this.props);
-    return (
-      <div className='wrapper'>
-        <Header />
-        <div className='content'>
-          <Route
-            path='/'
-            render={() => <Home items={this.props.items} />}
-            exact
-          />
-          <Route path='/cart' component={Cart} exact />
-        </div>
-      </div>
-    );
-  }
-}
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.pizzas.items,
+//   };
+// };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return { setPizzas: (items) => dispatch(setPizzasAction(items)) };
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return { setPizzas: (items) => dispatch(setPizzasAction(items)) };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,9 +25,7 @@ const Home = ({ items }) => {
       </div>
       <h2 className='content__title'>Все пиццы</h2>
       <div className='content__items'>
-        {items.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj} />
-        ))}
+        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
