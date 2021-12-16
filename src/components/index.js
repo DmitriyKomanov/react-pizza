2dd8da8491dfaa0354a68a7eadd9e0e7af23b7e6
komanov <komanov@twinscom.ru>
2021-12-17 00:36:10 +0300

    Компонент карточки пиццы в корзине

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,14 @@ import Categories from './Categories';
 import SortPopup from './SortPopup';
 import PizzaBlock from './PizzaBlock';
 import LoadingBlock from './PizzaBlock/LoadingBlock';
+import CartItem from './CartItem';
 
-export { Button, Header, Categories, SortPopup, PizzaBlock, LoadingBlock };
+export {
+  Button,
+  Header,
+  Categories,
+  SortPopup,
+  PizzaBlock,
+  LoadingBlock,
+  CartItem,
+};
